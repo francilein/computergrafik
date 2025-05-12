25f3cd672d27057e34c0bc2fb720420c85820f87
francilein <franca.lenz2004@gmail.com>
2025-05-12 18:08:17 +0200

    erste Klasse

---
+++ scene.cpp
@@ -0,0 +1,6 @@
+class Scene {
+    public :
+    std :: vector < Object *> objects ;
+    std :: vector <Light > lights ; // Lichter kommen später
+    Camera camera ; // Die Kamera kommt später hinzu
+};
